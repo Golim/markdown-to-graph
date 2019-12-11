04b47e1fb46c0d9b0245cf532050f61a700a558b
Matteo Golinelli <matteogolinelli97@gmail.com>
2019-12-11 17:56:54 +0100

    Add markdown-to-graph.py

---
+++ README.md
@@ -0,0 +1,38 @@
+# Markdown to Graph
+
+A completely useless program that draws a graph with the headers of a markdown file.
+
+**NOTE**: the program assumes that the markdown file is written according to the following rules:
+
+- There are no multiple top level headings in the same document
+- The first line of the document contains the top level header
+- Heading levels should only increment by one level at a time
+
+## Requirements
+
+- `python3`
+- [`dot`](https://linux.die.net/man/1/dot): to draw graphs
+
+## Usage
+
+`python markdown_to_graph.py input.md output.png`
+
+## Example
+
+```markdown
+# Header 1
+
+## Header 2
+
+### Header 3
+
+#### Header 4
+
+## Another header 2
+
+### Another header 3
+```
+
+Becomes
+
+![Example](graph.png)
---
+++ markdown-to-graph.py
@@ -0,0 +1,87 @@
+import subprocess
+import sys
+
+if len(sys.argv) < 2:
+    print("USAGE: python markdown_to_graph.py input.md output.png")
+    exit()
+
+markdown = open(sys.argv[1], "r")
+dot = open("graph.dot", "w")
+
+# Lines of the markdown file
+lines = markdown.readlines()
+markdown.close()
+
+# The graph of the markdown file
+graph = {
+    0: []
+}
+
+# Titles of nodes
+titles = {
+    0: lines[0].split("# ")[1].replace('\n', '')
+}
+
+# Contains the previous father node for every title level
+# Note that for simplicity I don't care that arrays starts at 0:
+#   I just don't use the first element of the array
+fathers = [0, 0, 0, 0, 0, 0, 0]
+
+def read_line(line_number, count):
+    global fathers, lines
+
+    # Skip non-title lines
+    while line_number < len(lines) and not lines[line_number].startswith('#'):
+            line_number += 1
+
+    # Check if in range
+    if line_number >= len(lines):
+        return
+
+    line = lines[line_number]
+
+    # If a line is a title
+    if line.startswith('#'):
+        count += 1
+
+        # Check which level of title is this line
+        level = len(line.split(" ")[0])
+
+        # This is the previous father for this level
+        fathers[level] = count
+
+        # Save the title for 'count' node
+        titles[count] = line.split("# ")[1].replace('\n', '')
+
+        # Add 'count' node to level - 1 father
+        #print("line", count, "is a child of", fathers[level - 1], " -> ", line)
+
+        if count not in graph:
+            graph[count] = []
+
+        graph[fathers[level - 1]].append(count)
+
+    read_line(line_number + 1, count)
+
+# Converts my graph in a DOT (graph description language) file
+def graph_to_dot():
+    dot.write("digraph markdown {\n")
+
+    # Iterate through keys in graph
+    for key in graph:
+        children = graph[key]
+
+        for child in children:
+            dot.write("    \"" + titles[key] + "\" -> \"" + titles[child] + "\";\n")
+    
+    dot.write("}")
+
+# Read from line 1, because line 0 contains the only h1
+read_line(1, 0)
+
+# Write the dot file
+graph_to_dot()
+dot.close()
+
+# Print the dot file to a png file
+subprocess.run(["dot", "-Tpng", "graph.dot", "-o", sys.argv[2]])
